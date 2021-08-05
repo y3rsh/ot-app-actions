d1559cb230aa313128abf6840a89dca0f98c0c75
Josh McVey <yersh13@gmail.com>
2021-08-05 14:47:02 -0500

    🔨

--- opentrons.rb
+++ opentrons.rb
@@ -1,9 +1,7 @@
 cask "opentrons" do
     name "opentrons"
     desc "opentrons app"
-    version "4.5.0"
-    homepage "https://s3.amazonaws.com"
-    url "https://s3.amazonaws.com/opentrons-app/builds/Opentrons-v4.5.0-mac-b12575.dmg"
+    url
     sha256 :no_check
     app "Opentrons.app"
 end
